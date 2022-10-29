3c246433a3ba1a63b7bb43add269785ccb3652ca
Akash <86942750+Akash2k15@users.noreply.github.com>
2022-10-29 20:32:53 +0530

    Update 0162-find-peak-element.cpp

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -7,6 +7,10 @@ public:
         while(low<=high){
             int mid = (low+high)/2;
             
+//             if an array has only one element then the element is the peak of the array
+//             if an array is sorted in ascending order then the last element is the peak and vice versa (there is atleast one peak element in an array)
+//             check mid if its peak, if not go to the half which is the highest valued neighbour of peak.
+            
             if((mid == 0 || nums[mid]>=nums[mid-1]) && (mid == nums.size()-1 || nums[mid]>=nums[mid+1])){
                 return mid;
             }
